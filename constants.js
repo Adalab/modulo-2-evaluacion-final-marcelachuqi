058a632ea208bdaac7cbe3bb109f787e065e16eb
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-23 00:55:47 +0200

    add js structure

---
+++ constants.js
@@ -0,0 +1,19 @@
+"use strict";
+globalData = [];
+
+const form = document.querySelector("form_js");
+const serieInput = document.querySelector(".search_js");
+const resultSerie = document.querySelector(".result_js");
+const SearchButton = document.querySelector(".button_js");
+const userSearch = serieInput.value.toLowerCase();
+
+// const serieValue = data;
+// function input(ev) {
+//   const serieValueUser = ev.target.serieInput;
+//   console.log(serieValueUser);
+// }
+
+// const liElement = document.createElement("li");
+// const newSerieList = document.createTextNode(`${data}`);
+// liElement.appendChild(newSerieList);
+// resultSerie.appendChild(liElement);
---
+++ main.js
@@ -0,0 +1,15 @@
+// //FUNCTIONS
+
+// function handleClickSearch() {
+//   fetch(`http://api.tvmaze.com/search/shows?q=${serieValueUser}`)
+//     .then((response) => response.json())
+//     .then((serieValue) => {
+//       if (serieValueUser !== serieValue) {
+//         resultSerie.innerHTML += ": Intenta buscar con otro nombre";
+//       } else {
+//         liElement = `<li>${serieValue}</li>`;
+//       }
+//     });
+// }
+
+// SearchButton.addEventListener("click", handleClickSearch);
