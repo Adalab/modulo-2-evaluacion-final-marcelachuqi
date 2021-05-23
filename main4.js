3b8a4d470514e092931af00db145bb260cb2ed2f
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-24 01:43:13 +0200

    add fetch

--- main4.js
+++ main4.js
@@ -4,44 +4,39 @@ const form = document.querySelector(".form_js");
 const serieInput = document.querySelector(".search_js");
 const btn = document.querySelector(".btn_js");
 const resultSerie = document.querySelector(".result_js");
+const SpanElement = document.querySelector(".img_js");
+let DivElement = document.createElement("div");
+let ImgElement = document.createElement("img");
+resultSerie.appendChild(ImgElement);
+resultSerie.appendChild(DivElement);
 
-let liElement = document.createElement("li");
-resultSerie.appendChild(liElement);
-
-function getSerie() {
-  const userSearch = serieInput.value;
+function getSerie(event) {
+  event.preventDefault();
+  const userSearch = serieInput.value.toLowerCase();
   let globalData = [];
 
-  fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
-    .then((response) => response.json())
-    .then((data) => {
-      data.forEach((element) => globalData.push(element.show.name));
-      console.log(globalData);
-      for (const name of globalData) liElement.innerHTML += `<li>${name}</li>`;
-    });
+  if (localStorage.getItem("name") === null) {
+    fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
+      .then((response) => response.json())
+      .then((data) => {
+        data.forEach((element) =>
+          globalData.push(
+            `${element.show.name}` +
+              " " +
+              ImgElement.setAttribute("src", `${element.show.image.medium}`)
+          )
+        );
+        for (const name of globalData)
+          DivElement.innerHTML += `<li>${name}</li>`;
+
+        console.log(globalData);
+      });
+  } else {
+    globalData = JSON.parse(localStorage.getItem("name"));
+    getSerie(globalData);
+  }
 }
 
+function getImage() {}
+
 btn.addEventListener("click", getSerie);
-
-// function renderPalettes(globalData) {
-//   {
-//   }
-// }
-
-// const paletteFav = favoritePalettes.find(
-//   (eachPalette) => eachPalette.id === palette.id
-// );
-
-// const classFavorite = paletteFav === undefined ? "" : "favorite";
-
-// paletteList.innerHTML += `
-//     <li id="${palette.id}" class="palette__list--item js-card ${classFavorite}">
-//       <h3 class="item__name">${palette.name}</h3>
-//       <p>Origen:${palette.from}</p>
-//       <div class="item__colors-box">
-//         <span>Colores:</span>
-//         <ul class="item__colors-list">
-//           ${htmlColors}
-//         </ul>
-//       </div>
-//     </li>`;
