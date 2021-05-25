c27e3909d71c2772f744aca7fc5b10d91f88d474
Marcela Chuqi <81633985+marcelachuqi@users.noreply.github.com>
2021-05-25 10:20:01 +0200

    add fav

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
-let globalData;
+let globalData = [];
+let favorites = [];
 
 const form = document.querySelector(".form_js");
 const serieInput = document.querySelector(".search_js");
@@ -12,27 +13,27 @@ resultSerie.appendChild(ImgElement);
 resultSerie.appendChild(DivElement);
 const userSearch = serieInput.value.toLowerCase();
 
+const favoriteStyle = document.querySelector(".favorites_js");
+let favElement = document.createElement("div");
+let favImgElement = document.createElement("img");
+favoriteStyle.appendChild(favElement);
+favoriteStyle.appendChild(favImgElement);
+
 function landing() {
-  let globalData = [];
   if (userSearch === "") {
     DivElement.innerHTML = `<h3 class="msj_js"> Let's start... </h3>`;
     fetch(`http://api.tvmaze.com/search/shows?q=serie`)
       .then((response) => response.json())
       .then((data) => {
-        data.forEach((element) =>
-          globalData.push(
-            `<div class="title_js">${element.show.name}</div>` +
-              " " +
-              `<img src="${element.show.image.medium}" />`
-          )
-        );
-
-        localStorage.setItem("show", JSON.stringify(globalData));
-        for (const name of globalData)
-          DivElement.innerHTML += `<li>${name}</li>`;
+        let data1 = data;
+        for (const name of data1)
+          DivElement.innerHTML +=
+            `<li>${name.show.name}</li>` +
+            " " +
+            `<img src="${name.show.image.medium}" />`;
       });
   } else {
-    console.log("refreshing");
+    console.log("buscando tus series...");
   }
 }
 landing();
@@ -42,126 +43,50 @@ function getSerie(event) {
   event.preventDefault();
   DivElement.innerHTML = `<li></li>`;
   const userSearch = serieInput.value.toLowerCase();
-  let globalData = [];
-  if (localStorage.getItem("name") === null) {
+  if (localStorage.getItem("show") !== "null") {
     fetch(`http://api.tvmaze.com/search/shows?q=${userSearch}`)
       .then((response) => response.json())
       .then((data) => {
-        data.forEach((element) =>
-          globalData.push(
-            `<div class="fav_js">${element.show.name}</div>` +
-              " " +
-              `<img src="${element.show.image.medium}" />`
-          )
-        );
+        globalData = data;
         localStorage.setItem("show", JSON.stringify(globalData));
-        for (const name of globalData)
-          DivElement.innerHTML += `<li>${name}</li>`;
+        RenderShow(globalData);
       });
   } else {
-    globalData = JSON.parse(localStorage.getItem("name"));
-    getSerie(globalData);
+    console.log("LocalStorage Empty");
   }
 }
 btn.addEventListener("click", getSerie);
 
-//FAVORITE
-function handlerfavorite(event) {
-  let serieClicked = event.target;
-  console.log("me han clicado");
-  console.log(serieClicked);
-}
-
-resultSerie.addEventListener("click", handlerfavorite);
-
-function addListenersToCards() {
-  const allCards = document.querySelectorAll(".js-card");
-  for (const card of allCards) {
-    card.addEventListener("click", handleClickCard);
+function RenderShow(globalData) {
+  for (const item of globalData) {
+    DivElement.innerHTML +=
+      `<li>${item.show.name}</li>` +
+      " " +
+      `<img src="${item.show.image.medium}" />` +
+      " " +
+      `<button class="favbutton_js" id=${item.show.id}> Agregar a favoritos </button>`;
   }
+  //   addListenersToSerie(globalData);
 }
 
-function handleClickCard(event) {
-  const whereTheUserClicked = event.target; // No hace falta
+function filterSerie() {
+  const filteredSerie = globalData.filter((show) =>
+    show.name.toLowerCase().includes(userSearch)
+  );
+  return filteredSerie;
+}
 
-  Identificar la li pulsada
-  const whereIAddedTheEvent = event.currentTarget;
-
-  Obtener la información asociada a la paleta
-  const paletteId = whereIAddedTheEvent.dataset.id;
-
-  Buscar si la paleta clickada está en favoritos
-  const isPresent = favorites.find((favoriteId) => favoriteId === paletteId);
-
-  if (isPresent === undefined) {
-    El ID de la paleta en la que ha hecho click no está en el array de favoritos
-    favorites.push(paletteId);
+function handleClickSerie(ev, globalData) {
+  let ShowID = ev.target.id;
+  console.log(ShowID);
+  localStorage.setItem("favorites", JSON.stringify(ShowID));
+  let favoriteId = " ";
+  const isFav = favorites.find((ShowID) => favoriteId === ShowID);
+  if (isFav === undefined) {
+    favorites.push(ShowID);
   } else {
-    favorites = favorites.filter((favoriteId) => favoriteId !== paletteId);
+    favorites = favorites.filter((favoriteId) => favoriteId !== ShowID);
   }
-
-  Re-pintamos las tarjetas de paletas teniéndo en cuenta el filtro.
-  renderFilteredPalettes();
+  console.log(favorites);
 }
-
-let globalData = [];
-let favorites = [];
-
-if (localStorage.getItem("palettes") === null) {
-  No tiene los datos guardados
-  fetch(
-    "https://beta.adalab.es/ejercicios-extra/js-ejercicio-de-paletas/data/palettes.json"
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      globalData = data.palettes;
-
-      setItem
-
-      localStorage.setItem("palettes", JSON.stringify(globalData));
-
-      renderPalettes(globalData);
-    });
-} else {
-  Sí tiene los datos guardados
-
-  globalData = JSON.parse(localStorage.getItem("palettes"));
-  renderPalettes(globalData);
-}
-
-function renderPalettes(data) {
-  paletteList.innerHTML = "";
-
-  for (const palette of data) {
-    let htmlColors = "";
-    for (let i = 0; i < palette.colors.length; i++) {
-      htmlColors += `<li class="item__colors-each" style="background-color:#${palette.colors[i]};"></li>`;
-    }
-
-    Buscar si la paleta que se está pintando está en favoritos
-    const isPresent = favorites.find((favoriteId) => favoriteId === palette.id);
-
-    Si el id está en favoritos, se renderiza el li con la clase favorite
-    let classFavorite = "";
-    if (isPresent === undefined) {
-      classFavorite = "";
-    } else {
-      classFavorite = "favorite";
-    }
-    Podría hacerse también con un ternario:
-    const classFavorite = ( isPresent === undefined ) ? '' : 'favorite';
-
-    paletteList.innerHTML += `
-      <li data-id="${palette.id}" class="palette__list--item js-card ${classFavorite}">
-        <h3 class="item__name">${palette.name}</h3>
-        <p>Origen:${palette.from}</p>
-        <div class="item__colors-box">
-          <span>Colores:</span>
-          <ul class="item__colors-list">
-            ${htmlColors}
-          </ul>
-        </div>
-      </li>`;
-  }
-  addListenersToCards();
-}
+resultSerie.addEventListener("click", handleClickSerie);
